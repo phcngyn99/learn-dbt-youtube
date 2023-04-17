596ed2a7474c93548c488f47ff68374f69a7d780
phuccnguyen <phuccnguyen99@gmail.com>
2023-04-17 18:03:34 +0700

    Add vars (variables) in both command line and project.yml

---
+++ target/compiled/demo_dbt/models/bq_ga4_shop/uploaded_shop_contains_3.sql
@@ -0,0 +1,14 @@
+with cte1 as (
+    select * from `shopkasatriavn`.`analytics_270502330`.`uploaded`
+),
+
+cte2 as(
+    select 
+        col1,col2,col3,
+        if(cte1.col3 = CAST('3' as INT64), 'Yes','No') as is_contains_3
+    from
+        cte1
+
+)
+
+select * from cte2
---
+++ target/run/demo_dbt/models/bq_ga4_shop/uploaded_shop_contains_3.sql
@@ -0,0 +1,25 @@
+
+  
+    
+
+    create or replace table `shopkasatriavn`.`staging`.`uploaded_shop_contains_3`
+    
+    
+    OPTIONS()
+    as (
+      with cte1 as (
+    select * from `shopkasatriavn`.`analytics_270502330`.`uploaded`
+),
+
+cte2 as(
+    select 
+        col1,col2,col3,
+        if(cte1.col3 = CAST('3' as INT64), 'Yes','No') as is_contains_3
+    from
+        cte1
+
+)
+
+select * from cte2
+    );
+  
